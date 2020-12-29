f71e4f97c389e3faa02a4edc2bb16bf6f14272a5
Nesy Widyastuti <56398122+NesyWidya24@users.noreply.github.com>
2020-12-29 12:24:13 +0700

    Feature Login

--- app/src/main/java/com/nessy/ecommersapp/MainSellerActivity.java
+++ app/src/main/java/com/nessy/ecommersapp/MainSellerActivity.java
@@ -1,15 +1,31 @@
 package com.nessy.ecommersapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class MainSellerActivity extends AppCompatActivity {
 
     private TextView nameTv;
     private ImageButton logoutBtn;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +34,68 @@ public class MainSellerActivity extends AppCompatActivity {
 
         nameTv = findViewById(R.id.nameTv);
         logoutBtn = findViewById(R.id.logoutBtn);
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        logoutBtn.setOnClickListener(v -> {
+            //make offline > sign out > go to login activity
+            makeMeOffline();
+        });
+    }
+
+    private void makeMeOffline() {
+        progressDialog.setMessage("Logging out...");
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("online", "false");
+
+        //update value to db
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.child(firebaseAuth.getUid()).updateChildren(hashMap)
+                .addOnSuccessListener(aVoid -> {
+                    //update successfully
+                    firebaseAuth.signOut();
+                    checkUser();
+                })
+                .addOnFailureListener(e -> {
+                    //failed updating
+                    progressDialog.dismiss();
+                    Toast.makeText(MainSellerActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    private void checkUser() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user == null) {
+            startActivity(new Intent(MainSellerActivity.this, LoginActivity.class));
+            finish();
+        } else {
+            loadMyInfo();
+        }
+    }
+
+    private void loadMyInfo() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.orderByChild("uid").equalTo(firebaseAuth.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            String name = "" + dataSnapshot.child("name").getValue();
+                            String accountType = "" + dataSnapshot.child("accountType").getValue();
+
+                            nameTv.setText(name + " (" + accountType + ")");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 }
--- app/src/main/java/com/nessy/ecommersapp/MainUserActivity.java
+++ app/src/main/java/com/nessy/ecommersapp/MainUserActivity.java
@@ -3,12 +3,16 @@ package com.nessy.ecommersapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -17,12 +21,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 public class MainUserActivity extends AppCompatActivity {
 
     private TextView nameTv;
     private ImageButton logoutBtn;
 
     private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +42,43 @@ public class MainUserActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+
         logoutBtn.setOnClickListener(v -> {
-            firebaseAuth.signOut();
-            checkUser();
+            //make offline > sign out > go to login activity
+            makeMeOffline();
         });
+    }
+
+    private void makeMeOffline() {
+        progressDialog.setMessage("Logging out...");
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("online", "false");
+
+        //update value to db
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.child(firebaseAuth.getUid()).updateChildren(hashMap)
+                .addOnSuccessListener(aVoid -> {
+                    //update successfully
+                    firebaseAuth.signOut();
+                    checkUser();
+                })
+                .addOnFailureListener(e -> {
+                    //failed updating
+                    progressDialog.dismiss();
+                    Toast.makeText(MainUserActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 
     private void checkUser() {
         FirebaseUser user = firebaseAuth.getCurrentUser();
-        if (user ==null){
+        if (user == null) {
             startActivity(new Intent(MainUserActivity.this, LoginActivity.class));
             finish();
-        }else {
+        } else {
             loadMyInfo();
         }
     }
@@ -57,11 +89,11 @@ public class MainUserActivity extends AppCompatActivity {
                 .addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        for (DataSnapshot dataSnapshot: snapshot.getChildren()){
-                            String name = ""+dataSnapshot.child("name").getValue();
-                            String accountType = ""+dataSnapshot.child("accountType").getValue();
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            String name = "" + dataSnapshot.child("name").getValue();
+                            String accountType = "" + dataSnapshot.child("accountType").getValue();
 
-                            nameTv.setText(name+" ("+accountType+")");
+                            nameTv.setText(name + " (" + accountType + ")");
                         }
                     }
 
