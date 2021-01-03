b1a36977c3fcec9a1b585d51c83eff9ab8816854
Nesy Widyastuti <56398122+NesyWidya24@users.noreply.github.com>
2021-01-03 20:14:03 +0700

    Feature Edit Profile

--- app/src/main/java/com/nessy/ecommersapp/ProfileEditUserActivity.java
+++ app/src/main/java/com/nessy/ecommersapp/ProfileEditUserActivity.java
@@ -1,20 +1,70 @@
 package com.nessy.ecommersapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ProfileEditUserActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
+import java.util.Locale;
+
+public class ProfileEditUserActivity extends AppCompatActivity implements LocationListener {
 
     private ImageButton backBtn, gpsBtn;
     private ImageView profileIv;
     private EditText nameEt, phoneEt, countryEt, stateEt, cityEt, addressEt;
     private Button updateBtn;
+
+
+    //permission const
+    private static final int LOCATION_REQUEST_CODE = 100;
+    private static final int CAMERA_REQUEST_CODE = 200;
+    private static final int STORAGE_REQUEST_CODE = 300;
+
+    //img, location pick const
+    private static final int IMG_PICK_GALLERY_CODE = 400;
+    private static final int IMG_PICK_CAMERA_CODE = 400;
+
+    //permission arrays
+    private String[] locationPermission;
+    private String[] cameraPermission;
+    private String[] storagePermission;
+
+    private Uri img_uri;
+
+    private double latitude = 0.0;
+    private double longitude = 0.0;
+
+    private ProgressDialog progressDialog;
+
+    //firebase auth
+    private FirebaseAuth firebaseAuth;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +82,230 @@ public class ProfileEditUserActivity extends AppCompatActivity {
         addressEt = findViewById(R.id.addressEt);
         updateBtn = findViewById(R.id.updateBtn);
 
+        //init permission arrays
+        locationPermission = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        //setup progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
         backBtn.setOnClickListener(v -> onBackPressed());
+
+        profileIv.setOnClickListener(v -> {
+            showImgPickDialog();
+        });
 
         gpsBtn.setOnClickListener(v -> {
             //detect location
+            if (!checkLocationPermission()) {
+                //already allowed
+                detectLocation();
+            } else {
+                //not allowed, request
+                requestLocationPermission();
+            }
         });
-
         updateBtn.setOnClickListener(v -> {
-
+            //update profile
         });
     }
+
+    private void showImgPickDialog() {
+        //option to display in dialog
+        String[] options = {"Camera", "Gallery"};
+        //dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick Image: ")
+                .setItems(options, (dialog, which) -> {
+                    if (which == 0) {
+                        if (checkCameraPermission()) {
+                            //allowed, open camera
+                            pickFromCamera();
+                        } else {
+                            //not allowed, request
+                            requestCameraPermission();
+                        }
+                    } else {
+                        if (checkStoragePermission()) {
+                            //allowed, open gallery
+                            pickFromGallery();
+                        } else {
+
+                            //not allowed, request
+                            requestStoragePermission();
+                        }
+                    }
+                }).show();
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this, storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, cameraPermission, CAMERA_REQUEST_CODE);
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, locationPermission, LOCATION_REQUEST_CODE);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) ==
+                (PackageManager.PERMISSION_GRANTED);
+
+        boolean result1 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                (PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+
+    private boolean checkLocationPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void pickFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMG_PICK_GALLERY_CODE);
+    }
+
+    private void pickFromCamera() {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, "Image Title");
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Image Description");
+
+        img_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, img_uri);
+        startActivityForResult(intent, IMG_PICK_CAMERA_CODE);
+    }
+
+    private void findAddress() {
+        //find address, country, state, city
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            String address = addresses.get(0).getAddressLine(0);
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+
+            //set address
+            countryEt.setText(country);
+            stateEt.setText(state);
+            cityEt.setText(city);
+            addressEt.setText(address);
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void detectLocation() {
+        Toast.makeText(this, "Please Wait....", Toast.LENGTH_SHORT).show();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+
+        findAddress();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        Toast.makeText(this, "Location is disabled...", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (locationAccepted) {
+                        detectLocation();
+                    } else {
+                        Toast.makeText(this, "Location permission is necessary...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            case CAMERA_REQUEST_CODE:{
+                if (grantResults.length > 0){
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted & storageAccepted){
+                        pickFromCamera();
+                    }else {
+                        Toast.makeText(this,"Camera permissions are necessary..", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            case STORAGE_REQUEST_CODE:{
+                if (grantResults.length > 0){
+                    boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (storageAccepted){
+                        pickFromGallery();
+                    }else {
+                        Toast.makeText(this,"Storage permission is necessary..", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //handle img pick result
+        if (resultCode == RESULT_OK){
+            if (requestCode == IMG_PICK_GALLERY_CODE){
+                img_uri = data.getData();
+                profileIv.setImageURI(img_uri);
+            }else if (requestCode == IMG_PICK_CAMERA_CODE){
+                profileIv.setImageURI(img_uri);
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
